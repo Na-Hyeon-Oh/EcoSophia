6ae56d1d9eb6704d381ab27b711978d9f91441ab
Na-Hyeon-Oh <onh0195@gmail.com>
2023-01-15 00:25:25 +0900

    [ADD] calendar today, selected, invalid(currentDT 달 외의 day) cell 처리

--- frontend/src/view/pages/home/calendar/custom/Calendar.tsx
+++ frontend/src/view/pages/home/calendar/custom/Calendar.tsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 import { Icon } from '@iconify/react';
 import { format, addMonths, subMonths } from 'date-fns';
 import { startOfMonth, endOfMonth, startOfWeek, endOfWeek } from 'date-fns';
-import { isSameMonth, isSameDay, addDays, parse } from 'date-fns';
+import { differenceInCalendarDays, isSameMonth, isSameDay, addDays, parse } from 'date-fns';
 import styles from './calendar.module.css'
 
 import { CalendarType } from '../../../../../assets/enums/CalendarType';
 
 const CustomCalender = (props: any) => {
-    const [option, setOption] = useState(CalendarType.MONTHLY);
-    const [currentMonth, setCurrentMonth] = useState(new Date());
+    const [todayDT, setTodayDT] = useState(new Date());
+    const [currentDT, setCurrentDT] = useState(new Date());
     const [selectedDate, setSelectedDate] = useState(new Date());
 
     const prevMonth = () : void => {
-        setCurrentMonth(subMonths(currentMonth, 1));
+        setCurrentDT(subMonths(currentDT, 1));
     };
     const nextMonth = () : void => {
-        setCurrentMonth(addMonths(currentMonth, 1));
+        setCurrentDT(addMonths(currentDT, 1));
     };
 
     const onDateClick = (day : Date) : void => {
@@ -25,21 +25,21 @@ const CustomCalender = (props: any) => {
 
     return (
         <div className={styles.custom_calender}>
-            {RenderHeader(currentMonth, prevMonth, nextMonth)}
+            {RenderHeader(currentDT, prevMonth, nextMonth)}
             {RenderDays()}
-            {RenderCells(currentMonth, selectedDate, onDateClick)}
+            {RenderCells(todayDT, currentDT, selectedDate, onDateClick)}
         </div>
     );
 };
 
-const RenderHeader = (currentMonth : Date, prevMonth : any, nextMonth : any) => {
+const RenderHeader = (currentDT : Date, prevMonth : any, nextMonth : any) => {
     return (
         <div className={styles.header_container}>
             <div className={styles.arrow_navigator}>
                 <Icon icon="bi:arrow-left-circle-fill" onClick={prevMonth} />
             </div>
             <div className={styles.middle}>
-                {format(currentMonth, 'yyyy.MM')}
+                {format(currentDT, 'yyyy.MM')}
             </div>
             <div className={styles.arrow_navigator}>
                 <Icon icon="bi:arrow-right-circle-fill" onClick={nextMonth} />
@@ -62,8 +62,8 @@ const RenderDays = () => {
     return <div className={styles.days_row}>{days}</div>;
 };
 
-const RenderCells = ( currentMonth : Date, selectedDate : Date, onDateClick : any ) => {
-    const monthStart = startOfMonth(currentMonth);
+const RenderCells = ( todayDT: Date, currentDT : Date, selectedDate : Date, onDateClick : any ) => {
+    const monthStart = startOfMonth(currentDT);
     const monthEnd = endOfMonth(monthStart);
     const startDate = startOfWeek(monthStart);
     const endDate = endOfWeek(monthEnd);
@@ -79,15 +79,15 @@ const RenderCells = ( currentMonth : Date, selectedDate : Date, onDateClick : an
             days.push(
                 <div
                     className = {styles.cell
-                        + ` ${isSameDay(day, currentMonth) ? 'today' : ''}`
+                        + ` ${differenceInCalendarDays(day, todayDT) == 0 ? styles.today : ''}`
                         + ` ${!isSameMonth(day, monthStart) ? '' 
-                            : isSameDay(day, selectedDate) ? 'selected' 
-                                : format(currentMonth, 'M') !== format(day, 'M') ? 'not-valid' : 'valid'}`}
+                            : isSameDay(day, selectedDate) ? styles.selected 
+                                : format(currentDT, 'M') !== format(day, 'M') ? styles.invalid : ''}`}
                     key = {day.toString()}
                     onClick = {() => onDateClick(cloneDay)}
                 >
                     <div className = {styles.cell_container}>
-                        <span className = {format(currentMonth, 'M') !== format(day, 'M') ? 'not-valid' : ''}>
+                        <span className = {format(currentDT, 'M') !== format(day, 'M') ? styles.invalid : ''}>
                             {formattedDate}
                         </span>
                     </div>
