33e26494d1bfc6879939eb458d66e006557f55fb
Na-Hyeon-Oh <onh0195@gmail.com>
2023-03-06 16:53:19 +0900

    [EDIT] DailyCalendar: MUI Accordion 적용

--- frontend/src/view/components/card/Card.tsx
+++ frontend/src/view/components/card/Card.tsx
@@ -1,5 +1,7 @@
 import Tag from '../../../model/Tag';
-import MultiSelect from '../../components/multiSelect/MultiSelect';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
 import Chip from '@mui/material/Chip';
 import ColorUtils from '../../../assets/utils/ColorUtils';
 
@@ -19,17 +21,21 @@ const Card = ({smallLeftText, smallRightText, leftText, rightText, tags, color} 
     }
 
     return (
-      <div className={styles.card} style={{borderColor:color}}>
-          <div className={styles.card_small_text}>
-              <div className={styles.left_text}>{smallLeftText}</div>
-              <div className={styles.right_text}>{smallRightText}</div>
-          </div>
-          <div className={styles.card_main_text}>
-              <div className={styles.left_text}>{leftText}</div>
-              <div className={styles.right_text}>{rightText}</div>
-          </div>
-          <div className={styles.card_tags}>{tagChips}</div>
-      </div>
+        <Accordion className={styles.card} style={{borderColor:color}}>
+            <AccordionSummary>
+                <div className={styles.card_main_text}>
+                    <div className={styles.left_text}>{leftText}</div>
+                    <div className={styles.right_text}>{rightText}</div>
+                </div>
+            </AccordionSummary>
+            <AccordionDetails>
+                <div className={styles.card_small_text}>
+                    <div className={styles.left_text}>{smallLeftText}</div>
+                    <div className={styles.right_text}>{smallRightText}</div>
+                </div>
+                <div className={styles.card_tags}>{tagChips}</div>
+            </AccordionDetails>
+        </Accordion>
     );
 }
 
