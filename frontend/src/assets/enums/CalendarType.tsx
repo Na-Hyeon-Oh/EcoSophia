8d18becad9ff8fdaab52d801edff3f13a0efc7ab
Lucy <64342804+Na-Hyeon-Oh@users.noreply.github.com>
2023-03-09 14:42:05 +0900

    Delete frontend directory

--- frontend/src/assets/enums/CalendarType.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// union types
-// ts enum is not recommended because of tree-shaking
-export const CalendarType = {
-    MONTHLY: "monthly",
-    WEEKLY: "weekly",
-    DAILY: "daily"
-}
